5f52fa3583249d40cb04a337a2f0e43de5c5a0ab
Léo Krebs <leokrebs68@gmail.com>
2020-07-15 20:47:57 +0200

    Models added
    - Python models added
    - File structures added

---
+++ src/server/models/album.py
@@ -0,0 +1,9 @@
+class Album:
+   def __init__(self, name, artist, words, genre, notation, year, yearListened):
+        self.name = name
+        self.artist = artist
+        self.words = words
+        self.genre = genre
+        self.notation = notation
+        self.year = year
+        self.yearListened = yearListened
---
+++ src/server/models/movie.py
@@ -0,0 +1,10 @@
+class Movie:
+   def __init__(self, title, director, words, atTheMovies, notation, year, yearSaw):
+        self.title = title
+        self.director = director
+        self.words = words
+        self.atTheMovies = atTheMovies
+        self.notation = notation
+        self.year = year
+        self.yearSaw = yearSaw
+
